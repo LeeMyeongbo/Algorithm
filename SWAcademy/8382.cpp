bd08fd2c349488e517424a2ebcf2f46f26b1a219
LeeMyeongbo <myeongbo123@naver.com>
2022-02-17 13:41:53 +0900

    update

---
+++ SWAcademy/8382.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int x, y, move;
+    bool garo;                  // garo : 가로 방향으로 이동했을 시 true, 세로 방향으로 이동했을 시 false
+};
+int T, start_x, start_y, dest_x, dest_y;
+bool visited[2][201][201];      // [0][r][c] : 가로 방향으로 도착 시 체크, [1][r][c] : 세로 방향으로 도착 시 체크
+
+int solve()
+{
+    queue<Node> q;
+    q.push({ start_x, start_y, 0, 0 });
+    q.push({ start_x, start_y, 0, 1 });
+    visited[0][start_y][start_x] = visited[1][start_y][start_x] = true;
+
+    while (!q.empty()) {
+        Node node = q.front();
+        q.pop();
+
+        if (node.x == dest_x && node.y == dest_y)
+            return node.move;
+
+        if (node.garo) {                                                // 가로 방향으로 왔을 경우 -> 세로로 이동
+            if (node.y - 1 >= 0 && !visited[1][node.y - 1][node.x]) {   // 북쪽으로 이동가능한지 살피고 이동
+                visited[1][node.y - 1][node.x] = true;
+                q.push({ node.x, node.y - 1, node.move + 1, false });
+            }
+            if (node.y + 1 < 201 && !visited[1][node.y + 1][node.x]) {  // 남쪽으로 이동가능한지 살피고 이동
+                visited[1][node.y + 1][node.x] = true;
+                q.push({ node.x, node.y + 1, node.move + 1, false });
+            }
+        }
+        else {                                                          // 세로 방향으로 왔을 경우 -> 가로로 이동
+            if (node.x - 1 >= 0 && !visited[0][node.y][node.x - 1]) {   // 서쪽으로 이동가능한지 살피고 이동
+                visited[0][node.y][node.x - 1] = true;
+                q.push({ node.x - 1, node.y, node.move + 1, true });
+            }
+            if (node.x + 1 < 201 && !visited[0][node.y][node.x + 1]) {  // 동쪽으로 이동가능한지 살피고 이동
+                visited[0][node.y][node.x + 1] = true;
+                q.push({ node.x + 1, node.y, node.move + 1, true });
+            }
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char** argv)
+{   
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; ++test_case) {
+        cin >> start_x >> start_y >> dest_x >> dest_y;
+        start_x += 100, start_y += 100, dest_x += 100, dest_y += 100;
+
+        cout << '#' << test_case << ' ' << solve() << "\n";
+        for (int i = 0; i < 201; i++) {
+            fill(visited[0][i], visited[0][i] + 201, false);
+            fill(visited[1][i], visited[1][i] + 201, false);
+        }
+    }
+    return 0;
+}
---
+++ SWAcademy/8382_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#define MAX 1<<30
+using namespace std;
+
+int T, start_x, start_y, dest_x, dest_y, dp[2][401][401];   // dp[0][r][c] : 가로로 도착 시 이동거리, dp[1][r][c] : 세로로 도착 시 이동거리
+
+int main(int argc, char** argv)
+{
+    for (int i = 0; i < 401; i++) {
+        fill(dp[0][i], dp[0][i] + 401, MAX);
+        fill(dp[1][i], dp[1][i] + 401, MAX);
+    }
+
+    dp[0][0][0] = dp[1][0][0] = 0;
+    for (int i = 1; i <= 400; i++) {    // 0번 행, 열 전처리
+        if (i & 1) {                    // 0번 행의 홀수 번째 열과 0번 열의 홀수 번째 행 초기값 설정
+            dp[1][i][0] = min(dp[1][i][0], dp[0][i - 1][0] + 1);
+            dp[0][0][i] = min(dp[0][0][i], dp[1][0][i - 1] + 1);
+        }
+        else {                          // 0번 행의 짝수 번째 열과 0번 열의 짝수 번째 행 초기값 설정
+            dp[0][i][0] = min(dp[0][i][0], dp[1][i - 1][0] + 3);
+            dp[1][0][i] = min(dp[1][0][i], dp[0][0][i - 1] + 3);
+        }
+    }
+
+    for (int r = 1; r <= 400; r++) {
+        for (int c = 1; c <= 400; c++) {
+            dp[0][r][c] = min(dp[0][r][c], dp[1][r][c - 1] + 1);
+            dp[1][r][c] = min(dp[1][r][c], dp[0][r - 1][c] + 1);
+        }
+    }
+    cin >> T;
+    
+    for (int test_case = 1; test_case <= T; ++test_case) {
+        cin >> start_x >> start_y >> dest_x >> dest_y;
+        dest_x = abs(dest_x - start_x);
+        dest_y = abs(dest_y - start_y);
+        start_x = start_y = 0;
+
+        cout << '#' << test_case << ' ' << min(dp[0][dest_y][dest_x], dp[1][dest_y][dest_x]) << "\n";
+    }
+    return 0;
+}
