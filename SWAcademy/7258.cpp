a187935b057d57405047ff79987b53d8768f8a10
LeeMyeongbo <myeongbo123@naver.com>
2022-02-27 01:47:19 +0900

    update

---
+++ SWAcademy/7258.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+using namespace std;
+
+int T, R, C, mem;
+char map[20][21];
+bool visited[16][4][20][20];
+
+bool dfs(int r, int c, int dir)             // dir : 0=서, 1=동, 2=북, 3=남
+{
+    if (visited[mem][dir][r][c])
+        return false;
+    visited[mem][dir][r][c] = true;
+
+    switch (map[r][c]) {
+    case '<':
+        return dfs(r, (c + C - 1) % C, 0);
+    case '>':
+        return dfs(r, (c + 1) % C, 1);
+    case '^':
+        return dfs((r + R - 1) % R, c, 2);
+    case 'v':
+        return dfs((r + 1) % R, c, 3);
+    case '_':
+        if (mem)
+            return dfs(r, (c + C - 1) % C, 0);
+        else
+            return dfs(r, (c + 1) % C, 1);
+    case '|':
+        if (mem)
+            return dfs((r + R - 1) % R, c, 2);
+        else
+            return dfs((r + 1) % R, c, 3);
+    case '?':                                   // '?'일 경우에는 어쩔 수 없이 4방향 모두 탐색해 봐야 함 (dfs로 해야하는 이유)
+        if (dfs(r, (c + C - 1) % C, 0))         // 프로그램을 정지할 수 있을 때까지 최대한 계속 해봄
+            return true;
+        if (dfs(r, (c + 1) % C, 1))
+            return true;
+        if (dfs((r + R - 1) % R, c, 2))
+            return true;
+        return dfs((r + 1) % R, c, 3);
+    case '.':
+        if (!dir)
+            return dfs(r, (c + C - 1) % C, 0);
+        else if (dir == 1)
+            return dfs(r, (c + 1) % C, 1);
+        else if (dir == 2)
+            return dfs((r + R - 1) % R, c, 2);
+        else
+            return dfs((r + 1) % R, c, 3);
+    case '@':
+        return true;
+    case '+':
+        mem = (mem + 1) % 16;
+        if (!dir)
+            return dfs(r, (c + C - 1) % C, 0);
+        else if (dir == 1)
+            return dfs(r, (c + 1) % C, 1);
+        else if (dir == 2)
+            return dfs((r + R - 1) % R, c, 2);
+        else
+            return dfs((r + 1) % R, c, 3);
+    case '-':
+        mem = (mem + 15) % 16;
+        if (!dir)
+            return dfs(r, (c + C - 1) % C, 0);
+        else if (dir == 1)
+            return dfs(r, (c + 1) % C, 1);
+        else if (dir == 2)
+            return dfs((r + R - 1) % R, c, 2);
+        else
+            return dfs((r + 1) % R, c, 3);
+    default:
+        mem = map[r][c] - '0';
+        if (!dir)
+            return dfs(r, (c + C - 1) % C, 0);
+        else if (dir == 1)
+            return dfs(r, (c + 1) % C, 1);
+        else if (dir == 2)
+            return dfs((r + R - 1) % R, c, 2);
+        else
+            return dfs((r + 1) % R, c, 3);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; ++test_case) {
+        cin >> R >> C;
+        for (int r = 0; r < R; r++)
+            cin >> map[r];
+
+        cout << '#' << test_case << ' ';
+        if (dfs(0, 0, 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
+        for (int a = 0; a < 16; a++)
+            for (int b = 0; b < 4; b++)
+                for (int c = 0; c < R; c++)
+                    for (int d = 0; d < C; d++)
+                        visited[a][b][c][d] = false;
+        mem = 0;
+    }
+    return 0;
+}
---
+++ SWAcademy/8191.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int T, N, idx[200001], Si;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> T;
+
+    for (int test_case = 1; test_case <= T; ++test_case) {
+        cin >> N;
+
+        for (int n = 1; n <= N; n++) {      // Si번 편수에 해당하는 index에 현재 순서 저장
+            cin >> Si;
+            idx[Si] = n;
+        }
+
+        int Ans = 1;
+        for (int i = 2; i <= N; i++)
+            if (idx[i] < idx[i - 1])        // Si번 편수의 만화책이 나타나는 순서가 Si-1번 편수 만화책보다 앞선다면 +1
+                Ans++;
+
+        cout << '#' << test_case << ' ' << Ans << "\n";
+    }
+    return 0;
+}
