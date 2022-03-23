7a74115c9ba9e79d51762480563f58bb1a577256
LeeMyeongbo <myeongbo123@naver.com>
2022-03-24 08:15:44 +0900

    update

---
+++ Baekjoon/14285.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int n, m, a, b, c, s, t, weight_sum, dij[2][5001];      // dij[0][i] : 간선 하나 삭제하지 않은 상태에서의 합, dij[1][i] : 하나 삭제한 상태에서의 합
+vector<pair<int, int>> graph[5001];                     // (연결된 정점, 가중치)로 저장
+
+int solve()
+{
+    priority_queue<pair<pair<int, int>, bool>> pq;
+    memset(dij, -1, sizeof(dij));
+    dij[0][s] = dij[1][s] = 0;
+    pq.push({ {dij[0][s], s}, false });
+    
+    while (!pq.empty()) {
+        int sum = -pq.top().first.first;
+        int cur = pq.top().first.second;
+        bool deleted = pq.top().second;
+        pq.pop();
+
+        for (auto& e : graph[cur]) {
+            if (!deleted) {                                                         // 간선이 아직 삭제되지 않았을 경우
+                if (dij[0][e.first] == -1 || dij[0][e.first] > sum + e.second) {    // 간선 삭제하지 않고 힙에 저장
+                    dij[0][e.first] = sum + e.second;
+                    pq.push({ {-dij[0][e.first], e.first}, false });
+                }
+                if (dij[1][e.first] == -1 || dij[1][e.first] > sum) {               // 간선 삭제하고 힙에 저장
+                    dij[1][e.first] = sum;
+                    pq.push({ {-sum, e.first}, true });
+                }
+            }
+            else {                                                                  // 이미 삭제되었을 경우
+                if (dij[1][e.first] == -1 || dij[1][e.first] > sum + e.second) {    // 그대로 삭제하지 않으면서 진행
+                    dij[1][e.first] = sum + e.second;
+                    pq.push({ {-dij[1][e.first], e.first}, true });
+                }
+            }
+        }
+    }
+
+    return weight_sum - dij[1][t];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    while (m--) {
+        cin >> a >> b >> c;
+        graph[a].push_back({ b, c });
+        graph[b].push_back({ a, c });
+        weight_sum += c;
+    }
+    cin >> s >> t;
+
+    cout << solve();
+    return 0;
+}
---
+++ Programmers/순위.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int getNum(int start, vector<int> graph[])
+{
+    queue<int> q;
+    bool visited[101] = { false, };
+    int cnt = 0;
+
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (int v : graph[cur]) {
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    vector<int> graph[101], inv_graph[101];
+
+    for (vector<int> v : results) {
+        graph[v[0]].push_back(v[1]);
+        inv_graph[v[1]].push_back(v[0]);
+    }
+    for (int i = 1; i <= n; i++) {
+        if (getNum(i, graph) + getNum(i, inv_graph) == n - 1)
+            answer++;
+    }
+    return answer;
+}
