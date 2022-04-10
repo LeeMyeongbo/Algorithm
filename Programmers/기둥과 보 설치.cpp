eb4bfb85dcca887988bb7a8b733d41aa3aefd66b
LeeMyeongbo <myeongbo123@naver.com>
2022-04-10 21:49:15 +0900

    update

---
+++ Programmers/기둥과 보 설치.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
+using namespace std;
+
+bool map[2][101][101];       // map[0][r][c] = true일 때 (r, c)~(r + 1, c)로 기둥 설치, map[1][r][c] = true일 때 (r, c)~(r, c + 1)로 보 설치
+list<pair<int, int>> gidung, bo;
+
+bool g_satisfied(bool g, int r, int c)      // (r, c)의 구조물을 지웠을 때 모든 기둥들이 조건을 만족하는지 살핌
+{
+    for (auto it = gidung.begin(); it != gidung.end(); ) {
+        int y = it->first, x = it->second;
+        if (g && y == r && x == c)
+            it = gidung.erase(it);
+        else if (!(!y || map[1][y][x - 1] || map[1][y][x] || map[0][y - 1][x]))
+            return false;
+        else
+            it++;
+    }
+    return true;
+}
+
+bool b_satisfied(bool b, int r, int c)      // (r, c)의 구조물을 지웠을 때 모든 보들이 조건을 만족하는지 살핌
+{
+    for (auto it = bo.begin(); it != bo.end(); ) {
+        int y = it->first, x = it->second;
+        if (b && y == r && x == c)
+            it = bo.erase(it);
+        else if (!(map[0][y - 1][x] || map[0][y - 1][x + 1] || (map[1][y][x + 1] && x - 1 >= 0 && map[1][y][x - 1])))
+            return false;
+        else
+            it++;
+    }
+    return true;
+}
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    vector<vector<int>> answer;
+    
+    for (auto& build : build_frame) {
+        int r = build[1], c = build[0], a = build[2], b = build[3];
+        if (!a && !b) {                                         // (r, c)~(r + 1, c)의 기둥 삭제 시
+            map[0][r][c] = false;
+            if (!g_satisfied(true, r, c) || !b_satisfied(false, r, c)) {
+                map[0][r][c] = true;
+                gidung.push_back({ r, c });
+            }
+        }
+        else if (!a && b) {                                     // (r, c)~(r + 1, c)에 기둥 설치 시
+            if (!r || map[1][r][c - 1] || map[1][r][c] || map[0][r - 1][c]) {
+                map[0][r][c] = true;
+                gidung.push_back({ r, c });                     // 바닥이거나 보의 한 쪽 끝 위에 있거나 다른 기둥 위에 있는 거라면 가능
+            }
+        }
+        else if (a && !b) {                                     // (r, c)~(r, c + 1)의 보 삭제 시
+            map[1][r][c] = false;
+            if (!g_satisfied(false, r, c) || !b_satisfied(true, r, c)) {
+                map[1][r][c] = true;
+                bo.push_back({ r, c });
+            }
+        }
+        else {                                                  // (r, c)~(r, c + 1)에 보 설치 시
+            if (map[0][r - 1][c] || map[0][r - 1][c + 1] || (map[1][r][c + 1]) && (c - 1 >= 0 && map[1][r][c - 1])) {
+                map[1][r][c] = true;
+                bo.push_back({ r, c });                         // 어느 한 곳에 기둥이 있거나 양쪽에 보가 설치되어 있을 경우 가능
+            }
+        }
+    }
+
+    for (int a = 0; a < 2; a++)
+        for (int r = 0; r <= n; r++)
+            for (int c = 0; c <= n; c++)
+                if (map[a][r][c]) {
+                    vector<int> tmp;
+                    tmp.push_back(c);
+                    tmp.push_back(r);
+                    tmp.push_back(a);
+                    answer.push_back(tmp);
+                }
+
+    sort(answer.begin(), answer.end());
+    return answer;
+}
+
+int main()
+{
+    int n, f, x, y, a, b;
+    vector<vector<int>> build_frame;
+    cin >> n >> f;
+
+    while (f--) {
+        vector<int> tmp;
+        cin >> x >> y >> a >> b;
+        tmp.push_back(x);
+        tmp.push_back(y);
+        tmp.push_back(a);
+        tmp.push_back(b);
+        build_frame.push_back(tmp);
+    }
+
+    vector<vector<int>> ans = solution(n, build_frame);
+
+    for (auto& a : ans)
+        cout << a[0] << ' ' << a[1] << ' ' << a[2] << '\n';
+
+    return 0;
+}
--- Programmers/표 편집.cpp
+++ Programmers/표 편집.cpp
@@ -1,124 +1,68 @@
-﻿#include <iostream>
-#include <string>
+﻿#include <string>
 #include <vector>
 #include <stack>
-#include <cmath>
 using namespace std;
 
-int tree[1 << 21], S;
-
-int getSum(int index, int start, int end, int left, int right)      // 해당 [start, end] 구간의 행 개수의 합 구함
-{
-    if (left > end || right < start)
-        return 0;
-    if (left >= start && right <= end)
-        return tree[index];
-
-    return getSum(index * 2, start, end, left, (left + right) / 2) + getSum(index * 2 + 1, start, end, (left + right) / 2 + 1, right);
-}
-
-int left_search(int sum, int k, int left, int right)    // x(0 <= x < k)부터 k-1까지의 행 개수 합이 sum을 만족하는 가장 큰 x 구함
-{
-    int ans = 0;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        int Sum = getSum(1, mid, k - 1, 0, S - 1);
-        if (Sum >= sum) {
-            ans = mid;
-            left = mid + 1;
-        }
-        else
-            right = mid - 1;
-    }
-    return ans;
-}
-
-int right_search(int sum, int k, int left, int right)   // k+1부터 x(k < x < n)까지의 행 개수 합이 sum을 만족하는 가장 작은 x 구함
-{
-    int ans = 0;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        int Sum = getSum(1, k + 1, mid, 0, S - 1);
-        if (Sum >= sum) {
-            ans = mid;
-            right = mid - 1;
-        }
-        else
-            left = mid + 1;
-    }
-    return ans;
-}
+struct Node {
+    int num;
+    Node* prev, * next;
+} node[1000000];
+bool deleted[1000000];
 
 string solution(int n, int k, vector<string> cmd) {
     string answer = "";
-    stack<int> s;
-    S = 1 << (int)ceil(log2(n));
-    for (int i = S; i < n + S; i++)                 // 말단노드 [S, n + S) 중에서 삭제된 것은 0, 삭제 안 된 건 1로 저장
-        tree[i] = 1;
+    stack<Node*> s;
 
-    for (int i = S - 1; i > 0; i--)
-        tree[i] = tree[i * 2] + tree[i * 2 + 1];
-    
-    for (string c : cmd) {
+    Node* now = node + k;
+    node[0] = { 0, nullptr, node + 1 };
+    for (int i = 1; i < n - 1; i++)
+        node[i] = { i, node + i - 1, node + i + 1 };
+    node[n - 1] = { n - 1, node + n - 2, nullptr };
+
+    for (string& c : cmd) {
         if (c[0] == 'U' || c[0] == 'D') {
             string X;
             X.assign(c, 2, c.length() - 2);
             int x = stoi(X);
 
-            k = c[0] == 'U' ? left_search(x, k, 0, k - 1) : right_search(x, k, k + 1, n - 1);
+            if (c[0] == 'U')
+                while (x--)
+                    now = now->prev;
+            else
+                while (x--)
+                    now = now->next;
         }
         else if (c[0] == 'C') {
-            int idx = S + k;
-            while (idx) {
-                tree[idx]--;
-                idx /= 2;
-            }
-            s.push(k);
+            if (now->prev)
+                now->prev->next = now->next;
+            if (now->next)
+                now->next->prev = now->prev;
 
-            if (!getSum(1, k + 1, S - 1, 0, S - 1))         // 마지막 행을 삭제했을 경우 
-                k = left_search(1, k, 0, k - 1);            // k 이전 행들 중에서 k와 가장 가까운 이전 행 탐색
-            else                                            // k 이후 행들이 남아있을 경우
-                k = right_search(1, k, k + 1, n - 1);       // k 이후 행들 중에서 k와 가장 가까운 이후 행 탐색
+            deleted[now->num] = true;
+            s.push(now);
+            if (now->next)
+                now = now->next;
+            else
+                now = now->prev;
         }
         else {
-            int rollback = s.top();
-            int idx = S + rollback;
-            while (idx) {
-                tree[idx]++;
-                idx /= 2;
-            }
+            Node* node = s.top();
             s.pop();
+
+            if (node->prev)
+                node->prev->next = node;
+            if (node->next)
+                node->next->prev = node;
+            deleted[node->num] = false;
         }
     }
 
-    for (int i = S; i < n + S; i++) {
-        if (tree[i])
-            answer.push_back('O');
-        else
+    for (int i = 0; i < n; i++) {
+        if (deleted[i])
             answer.push_back('X');
+        else
+            answer.push_back('O');
     }
 
     return answer;
-}
-
-int main()
-{
-    int n, k, c;
-    string cm, X;
-    vector<string> cmd;
-
-    cin >> n >> k >> c;
-    while (c--) {
-        cin >> cm;
-        if (cm == "U" || cm == "D") {
-            cin >> X;
-            cm.append(" ");
-            cm.append(X);
-        }
-        cmd.push_back(cm);
-    }
-
-    cout << solution(n, k, cmd);
-
-    return 0;
 }
