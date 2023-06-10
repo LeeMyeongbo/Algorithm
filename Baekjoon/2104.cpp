763499cf54260734e91ae8f1dfedd97816d9e9bc
LeeMyeongbo <myeongbo123@naver.com>
2023-06-11 01:25:50 +0900

    update

---
+++ Baekjoon/2104.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+unsigned int N, S, leftTree[1 << 18], rightTree[1 << 18];
+long long sumTree[1 << 18];
+
+long long search(int index, int left, int right, int& r1, int& r2)
+{
+    if (left > r2 || right < r1)
+        return 0;
+
+    if (left >= r1 && right <= r2)
+        return sumTree[index];
+
+    int mid = (left + right) >> 1;
+    return search(index << 1, left, mid, r1, r2) + search((index << 1) + 1, mid + 1, right, r1, r2);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    memset(rightTree, UINT32_MAX, sizeof(rightTree));
+    memset(leftTree, UINT32_MAX, sizeof(leftTree));
+
+    cin >> N;
+    S = 1 << (int)ceil(log2(N));
+
+    for (register int i = 0; i < N; ++i)
+    {
+        register int idx = S + i;
+
+        cin >> rightTree[idx];
+        sumTree[idx] = rightTree[S + i];
+        
+        while (idx >> 1)
+        {
+            sumTree[idx >> 1] += rightTree[S + i];
+            rightTree[idx >> 1] = min(rightTree[idx >> 1], rightTree[idx]);
+            idx >>= 1;
+        }
+    }
+    
+    long long ans = 0;
+    for (register int i = 0; i < N; ++i)
+    {
+        register int idx = S + i, cur = rightTree[idx], leftidx = 0, rightidx = N + 1;
+
+        leftTree[idx] = cur;
+        while (idx >> 1)
+        {
+            leftTree[idx >> 1] = min(leftTree[idx >> 1], leftTree[idx]);
+            idx >>= 1;
+        }
+
+        // 현재 A[i]보다 왼쪽에 있는 A값들 중 A[i]보다 작으면서 가장 오른쪽에 있는 A값의 위치 구함
+        register int index = 1;
+        if (leftTree[index] < cur)
+        {
+            while (index < S)
+            {
+                if (leftTree[(index << 1) + 1] < cur)
+                    index = (index << 1) + 1;
+                else
+                    index <<= 1;
+            }
+            leftidx = index - S + 1;
+        }
+
+        // 현재 A[i]보다 오른쪽에 있는 A값들 중 A[i]보다 작으면서 가장 왼쪽에 있는 A값의 위치 구함
+        index = 1;
+        if (rightTree[index] < cur)
+        {
+            while (index < S)
+            {
+                if (rightTree[index << 1] < cur)
+                    index <<= 1;
+                else
+                    index = (index << 1) + 1;
+            }
+            rightidx = index - S + 1;
+        }
+
+        idx = S + i;
+        rightTree[idx] = UINT32_MAX;
+        idx >>= 1;
+        while (idx)
+        {
+            rightTree[idx] = min(rightTree[idx << 1], rightTree[(idx << 1) + 1]);
+            idx >>= 1;
+        }
+
+        ans = max(ans, search(1, 1, S, ++leftidx, --rightidx) * cur);
+    }
+
+    cout << ans;
+
+    return 0;
+}
---
+++ Baekjoon/2104_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int N, A[100002];
+long long sum[100002];
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> N;
+
+    stack<int> s;
+    register long long ans = 0;
+
+    for (register int i = 1; i <= N + 1; ++i)
+    {
+        if (i <= N)
+        {
+            cin >> A[i];
+            sum[i] = sum[i - 1] + A[i];
+        }
+        while (!s.empty() && A[s.top()] > A[i])
+        {
+            long long h = A[s.top()];
+            s.pop();
+
+            int leftidx = s.empty() ? 0 : s.top();
+            ans = max(ans, (sum[i - 1] - sum[leftidx]) * h);
+        }
+        s.emplace(i);
+    }
+
+    cout << ans;
+
+    return 0;
+}
